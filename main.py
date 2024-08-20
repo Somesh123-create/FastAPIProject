752922684d78db5706d5a840b10f14dc544db91e
Somesh Hiremath <somesh.hiremath@yash.com>
2024-08-21 01:23:16 +0530

    db connetion

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routers import get_blogs, post_blogs
-
+from db import models
+from db.database import engine
 
 app = FastAPI()
 
@@ -11,3 +12,6 @@ app.include_router(post_blogs.router)
 @app.get('/')
 def index():
     return {"message":"Hello World!"}
+
+
+models.Base.metadata.create_all(engine)

e22b39de52337d89d865315faca69e72a487dee2
Somesh Hiremath <somesh.hiremath@yash.com>
2024-08-22 02:00:13 +0530

    curd foregin key

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from routers import get_blogs, post_blogs, user
+from routers import get_blogs, post_blogs, user, article
 from db import models
 from db.database import engine
 
 app = FastAPI()
 
 app.include_router(user.router)
+app.include_router(article.router)
 app.include_router(get_blogs.router)
 app.include_router(post_blogs.router)
 
--- schemas.py
+++ schemas.py
@@ -1,5 +1,13 @@
 from pydantic import BaseModel
 
+#article inside user display
+class Article(BaseModel):
+    title : str
+    content : str
+    published : bool
+    class Config():
+        orm_model: True
+
 class UserBase(BaseModel):
     username: str
     email: str
@@ -8,5 +16,30 @@ class UserBase(BaseModel):
 class UserDisplay(BaseModel):
     username: str
     email: str
+    itmes: list[Article] = []
+    class Config():
+        orm_model: True
+
+
+class ArticleBase(BaseModel):
+    title: str
+    content: str
+    published: bool
+    creator_id: int
+
+
+#user inside article display
+class User(BaseModel):
+    id: int
+    username: str
+    class Config():
+        orm_model: True
+
+
+class ArticleDisplay(BaseModel):
+    title: str
+    content: str
+    published: bool
+    user: User
     class Config():
         orm_model: True

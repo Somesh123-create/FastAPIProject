b2e14c28cc98625138ae442a1e609fafb6323b38
Somesh Hiremath <somesh.hiremath@yash.com>
2024-08-27 00:23:17 +0530

    fast api update

--- db/db_user.py
+++ db/db_user.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm.session import Session
 from schemas import UserBase
 from db.models import DbUser
 from db.hash import Hash
+from fastapi import HTTPException, status
 
 def create_user(db: Session, request: UserBase):
     new_user = DbUser(
@@ -20,11 +21,16 @@ def get_all_users(db: Session):
 
 
 def get_user(db: Session, id: int):
-    return db.query(DbUser).filter(DbUser.id == id).first()
+    user = db.query(DbUser).filter(DbUser.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"user with id {id} not found.")
+    return user
 
 
 def update_user(db: Session, id: int, request: UserBase):
-    user = db.query(DbUser).filter(DbUser.id == id)
+    user = db.query(DbUser).filter(DbUser.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"user with id {id} not found.")
     user.update({
         DbUser.username : request.username,
         DbUser.email : request.email,
@@ -35,6 +41,8 @@ def update_user(db: Session, id: int, request: UserBase):
 
 def delete_user(db: Session, id: int):
     user = db.query(DbUser).filter(DbUser.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"user with id {id} not found.")
     db.delete(user)
     db.commit()
     return "deleted"
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from routers import get_blogs, post_blogs, user, article
 from db import models
 from db.database import engine
+from exceptions import StoryException
+from fastapi.responses import JSONResponse
+from fastapi import Request
 
 app = FastAPI()
 
@@ -15,5 +18,12 @@ app.include_router(post_blogs.router)
 def index():
     return {"message":"Hello World!"}
 
+
+@app.exception_handler(StoryException)
+def story_exception_handler(request: Request, exc: StoryException):
+    return JSONResponse(
+        status_code=418,
+        content = {"detail": exc.name}
+    )
 
 models.Base.metadata.create_all(engine)
